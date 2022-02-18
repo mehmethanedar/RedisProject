b0e0911223592dc2ffb8b726d552b5377b6cdb78
Mehmet HANEDAR <mehmethanedar@hotmail.com>
2022-02-18 16:57:07 +0300

    Initial

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _distributedCache.SetString("name", "Mehmet", cacheEntryOptions);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var name = _distributedCache.GetString("name");
+            ViewBag.Name = name;
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+            return View();
+        }
+    }
+}
